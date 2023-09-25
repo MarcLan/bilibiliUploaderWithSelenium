ab3adec13d9ec2b0bfc50052361fe276c91e1084
Marc <marclan8728@gmail.com>
2023-09-25 08:31:52 -0700

    Added feature of don't close browser

--- testFirstScript.py
+++ testFirstScript.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options= chrome_options)
 driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
 title = driver.title
-driver.implicitly_waite(0.5)
+driver.implicitly_wait(100)
 
 text_box = driver.find_element(by = By.NAME, value= "my-text")
 submit_button = driver.find_element(by = By.CSS_SELECTOR, value = "button")
@@ -14,7 +18,8 @@ text_box.send_keys("Selenium")
 submit_button.click()
 
 message = driver.find_element(by = By.ID, value = "message")
-value = message.txt
+value = message.text
 assert value == "Received!"
 
-driver.quit()
+driver.implicitly_wait(60)
+
